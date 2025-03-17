c42e3afec9d255cce9e47816b326d905333a4567
Mateo Ramos <mramos5@miuandes.cl>
2025-03-17 18:34:14 -0300

    Boton hace más que solo cambio de texto

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function changeText() {
     var newText = document.getElementById("inputText").value;
     document.getElementById("changeableText").innerText = newText;
+
+    var randomColor = '#' + Math.floor(Math.random() * 16777218).toString(16);
+
+    document.querySelector(".container").style.backgroundColor = randomColor;
 }
